8b61d152db25adb2d4470e253eae426ece1923ec
Dave Green <greendavegreen@gmail.com>
2016-06-22 09:22:16 -0400

    parse lib updates and inclusion into jomaCL

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
 var nu = require('elementa_name_utils');
+var pu = require('elementa-parse-utils');
+
+
+var xml = 'hi';
+var article = pu.newArticle(xml);
 
 if (nu.isValidAsset('elementa.000001.xml')) {
   console.log('is asset');
 }
+
 
 console.log('Hello, world!');
